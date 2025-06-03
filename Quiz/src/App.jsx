c9f7c2c8ec338ac3a4783f87e91ee4aa02494cfe
JoaresMiranda <joaresmiranda@gmail.com>
2025-06-03 16:22:23 -0300

    Criando o componente inicial

--- Quiz/src/App.jsx
+++ Quiz/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div>
       <h1>Quiz de Programação</h1>
+      <Welcome />
     </div>
   );
 }
